e4d2ddd103aaf5dfd026f88f0ed115e9dbe96395
Juan Fernandez <jsebas1198@protonmail.com>
2022-03-09 13:42:23 -0300

    the token containing the informatios ofthe user is send and at login the user is redirected to dashboard

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,5 @@
-import React, { useContext, createContext } from 'react';
+import React, { useState, useContext, createContext } from 'react';
+import Cookie from 'js-cookie';
 import axios from 'axios';
 import endPoints from '@services/api/';
 
@@ -14,6 +15,8 @@ export const useAuth = () => {
 };
 
 function useProvideAuth() {
+  const [user, setUser] = useState(null);
+
   const signIn = async (email, password) => {
     const options = {
       headers: {
@@ -22,10 +25,17 @@ function useProvideAuth() {
       },
     };
     const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
-    console.log(access_token);
+    if (access_token) {
+      const token = access_token.access_token;
+      Cookie.set('token', token, { expires: 5 });
+      axios.defaults.headers.Authorization = `Bearer ${token}`;
+      const { data: user } = await axios.get(endPoints.auth.profile);
+      setUser(user);
+    }
   };
 
   return {
+    user,
     signIn,
   };
 }
